c6d8ff5c96649cf1ad69fb3b9d05fbbb8023bd55
Bairui Su <subairui@icloud.com>
2024-06-12 21:38:44 +0800

    Next (#62)

--- test/apps/expression.js
+++
@@ -1,7 +0,0 @@
-import {html, state} from "../../src/index.js";
-
-export function expression() {
-  return html`<define text=${state("hello world")}>
-    <span>${(d) => d.text}</span>
-  </define>`;
-}
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vitest/config";
+import path from "path";
 
 export default defineConfig({
-  root: "./test",
+  test: {
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      echox: path.resolve("./src/index.js"),
+    },
+  },
 });
