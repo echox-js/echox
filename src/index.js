3f332f3b52d48b25b374f2d8c36c26d367db2b2d
pearmini <subairui@icloud.com>
2024-06-13 11:37:23 +0800

    Add Echo.X, Echo.component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export function render() {}
+const node = (tag, ns) => (props, _) => Object.assign((_ = (...c) => ((_.children = c), _)), {props, tag, ns});
+
+const handler = (ns) => ({get: (_, tag) => node(tag, ns)});
+
+export const X = new Proxy((ns) => new Proxy({}, handler(ns)), handler());
+
+export const component = (...args) => node(args);
+
+export default {X, component};
