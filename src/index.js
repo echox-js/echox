73ea26b6158a4b0e553a2cec1011de91021c3289
MiniPear <pearminipro@gmail.com>
2024-06-18 22:04:39 +0800

    Bind state to child

--- src/index.js
+++ src/index.js
@@ -2,32 +2,44 @@ const protoOf = Object.getPrototypeOf;
 const strProto = protoOf("");
 const funcProto = protoOf(protoOf);
 const isStr = (d) => protoOf(d) === strProto;
-const isFunc = (d) => protoOf(d) === funcProto;
+const isFunc = (d) => protoOf(d) === funcProto && !d.tag;
 const unset = Symbol();
 
 const cache = {};
-let updates, actives;
+let updates, actives, disposes;
+
+const schedule = (set, state, callback) => (set ?? (setTimeout(callback), new Set())).add(state);
+
+const cleanup = (state) => ((state.effects = new Set([...state.effects].filter((e) => e._el.isConnected))), state);
 
 function scheduleUpdate(state) {
-  updates = (updates ?? (setTimeout(flushUpdate), new Set())).add(state);
+  updates = schedule(updates, state, () => {
+    const called = new Set();
+    updates.forEach((s) => cleanup(s).effects.forEach((e) => called.has(e) || (called.add(e), track(e, e._el))));
+    updates = null;
+  });
 }
 
-function flushUpdate() {
-  const called = new Set();
-  updates.forEach(({effects}) => effects.forEach((e) => called.has(e) || (called.add(e), track(e))));
-  updates = null;
+function scheduleCleanup(state) {
+  disposes = schedule(disposes, state, () => {
+    disposes.forEach((s) => cleanup(s));
+    disposes = null;
+  });
+  return state;
 }
 
-function track(effect) {
+function track(effect, el = {isConnected: true}) {
   const prev = actives;
   const cur = (actives = {setters: new Set(), getters: new Set()});
+  let newEl;
   try {
-    effect();
+    newEl = effect();
   } catch (e) {
     console.error(e);
   }
+  effect._el = newEl?.nodeType ? (effect._el?.replaceWith?.(newEl), newEl) : el;
   actives = prev;
-  for (const d of cur.getters) cur.setters.has(d) || d.effects.add(effect);
+  for (const d of cur.getters) cur.setters.has(d) || scheduleCleanup(d).effects.add(effect);
 }
 
 export function reactive() {
@@ -71,10 +83,10 @@ function render(node, data = {}) {
   for (const [k, v] of Object.entries(props)) {
     const prop = (cache[tag + "," + k] ??= setterOf(el, k)?.set ?? 0).bind?.(el) ?? el.setAttribute.bind(el, k);
     const event = (v) => el.addEventListener(k.slice(1), v);
-    const state = (v) => track(() => prop(v(data)));
+    const state = (v) => track(() => prop(v(data)), el);
     (k.startsWith("$") ? event : isFunc(v) ? state : prop)(v);
   }
-  for (const child of children) el.appendChild(render(child, data));
+  for (let c of children) isFunc(c) ? track(() => render(c(data))) : render(c);
   return el;
 }
 
