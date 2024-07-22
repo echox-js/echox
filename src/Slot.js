0bc89455d4d8a778bdca89a4990ca8c42ed2f116
MiniPear <pearminipro@gmail.com>
2024-07-22 22:26:50 +0800

    Refactor Slot

--- src/Slot.js
+++ src/Slot.js
@@ -2,10 +2,7 @@ import {controlFlow} from "./controlFlow.js";
 import {mount} from "./mount.js";
 import {reactive} from "./reactive.js";
 
-export const Slot = controlFlow(
-  reactive().get("from", (d) => () => d.children),
-  (d, parent) => {
-    const fromNodes = [d.from].flat(Infinity);
-    (fromNodes.length ? fromNodes : d.children).forEach((child) => mount(parent, child));
-  },
-);
+export const Slot = controlFlow(reactive().get("from"), (d, parent) => {
+  const fromNodes = [d.from].flat(Infinity);
+  (fromNodes.length ? fromNodes : d.children).forEach((child) => mount(parent, child));
+});
