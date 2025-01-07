ee40486dc957282b4a9d6fd2062b2aaf649ed5bb
pearmini <subairui@icloud.com>
2025-01-06 21:44:00 -0500

    Only save css and cx

--- test/container.js
+++
@@ -1,6 +0,0 @@
-export async function withContainer(callback) {
-  const div = document.createElement("div");
-  document.body.appendChild(div);
-  await callback(div);
-  div.remove();
-}
--- test/style.spec.js
+++ test/style.spec.js
@@ -1,18 +1,24 @@
-import * as EchoX from "echox";
 import {test, expect} from "vitest";
-import {withContainer} from "./container.js";
+import {css, cx} from "../src/style";
 
-const {html} = EchoX;
+test("css should convert style objects to CSS string", () => {
+  expect(css({backgroundColor: "red"})).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, {color: "blue"})).toBe("color: blue;background-color: red");
+  expect(css({backgroundColor: "red"}, false && {color: "blue"})).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, null)).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, undefined)).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, 0)).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, "")).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, [])).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, {})).toBe("background-color: red");
+  expect(css({backgroundColor: "red"}, new Date())).toBe("background-color: red");
+});
 
-test("cx and css should render style conditionally", async () => {
-  await withContainer(async (el) => {
-    const App = EchoX.component(
-      html.div({
-        class: EchoX.cx(null, "a", undefined, new Date(), {b: true}, {c: false, d: true, e: new Date()}),
-        style: EchoX.css({backgroundColor: "red"}, {backgroundColor: "blue"}, false && {backgroundColor: "yellow"}),
-      })("Hello, World!"),
-    );
-    EchoX.mount(el, App());
-    expect(el.innerHTML).toBe(`<div class="a b d e" style="background-color: red;">Hello, World!</div>`);
-  });
+test("cx should combine class names conditionally", () => {
+  expect(cx("a", "b")).toBe("a b");
+  expect(cx("a", false && "b", "c")).toBe("a c");
+  expect(cx({a: true, b: false, c: true})).toBe("a c");
+  expect(cx("a", {b: true, c: false, d: true})).toBe("a b d");
+  expect(cx(null, "a", undefined, new Date(), {b: true}, {c: false, d: true, e: new Date()})).toBe("a b d e");
+  expect(cx("a", null, undefined, 0, "", [], {})).toBe("a");
 });
