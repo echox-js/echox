c2d88f96f36d13f66dbe0788ed9318b18195879e
pearmini <subairui@icloud.com>
2025-01-06 21:44:00 -0500

    Only save css and cx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-export {Arm} from "./Arm.js";
-export {component} from "./component.js";
-export {css} from "./css.js";
-export {cx} from "./cx.js";
-export {For} from "./For.js";
-export {Fragment} from "./Fragment.js";
-export {html} from "./html.js";
-export {Match} from "./Match.js";
-export {mount} from "./mount.js";
-export {reactive} from "./reactive.js";
-export {Slot} from "./Slot.js";
-export {unmount} from "./unmount.js";
+export {css, cx} from "./style.js";
