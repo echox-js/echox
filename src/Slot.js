9399eeb90b5076d0cf4dc30b100e28ac1f9b841a
pearmini <subairui@icloud.com>
2025-01-06 21:44:00 -0500

    Only save css and cx

--- src/Slot.js
+++
@@ -1,8 +0,0 @@
-import {controlFlow} from "./controlFlow.js";
-import {h} from "./mount.js";
-import {reactive} from "./reactive.js";
-
-export const Slot = controlFlow(reactive().get("from"), (d, parent) => {
-  const fromNodes = [d.from].flat(Infinity);
-  (fromNodes.length ? fromNodes : d.children).forEach((child) => h(parent, child));
-});
