90cc18f31f9b116b0c621c774c45b8608706318b
Bairui Su <subairui@icloud.com>
2025-02-23 15:00:41 -0500

    Update logo and color (#162)
    * Update logo and color
    
    * Add docs folder
    
    * Update logo.png

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,40 +5,41 @@ export default defineConfig({
   title: "EchoX",
   description: "UI = f(reactive, template)",
   cleanUrls: true,
-  head: [["link", {rel: "icon", type: "image/svg+xml", href: "/logo.svg"}]],
+  head: [["link", {rel: "icon", type: "image/png", href: "/logo.png"}]],
+  appearance: false,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      {text: "Docs", link: "/what-is-echox"},
+      {text: "Docs", link: "/docs/what-is-echox"},
       // {text: "Blogs", link: "/blog/index"},
     ],
     sidebar: {
-      "/": [
+      "/docs": [
         {
           text: "Introduction",
           items: [
-            {text: "What is EchoX", link: "/what-is-echox"},
-            {text: "Why is EchoX", link: "/why-is-echox"},
-            {text: "Getting Started", link: "/getting-started"},
-            {text: "API Index", link: "/api-index"},
+            {text: "What is EchoX", link: "/docs/what-is-echox"},
+            {text: "Why is EchoX", link: "/docs/why-is-echox"},
+            {text: "Getting Started", link: "/docs/getting-started"},
+            {text: "API Index", link: "/docs/api-index"},
           ],
         },
         {
           text: "Reference",
           items: [
-            {text: "EchoX DOM", link: "/echox-dom"},
-            {text: "EchoX Reactive", link: "/echox-reactive"},
+            {text: "EchoX DOM", link: "/docs/echox-dom"},
+            {text: "EchoX Reactive", link: "/docs/echox-reactive"},
           ],
         },
       ],
       "/blog/": [],
     },
-    socialLinks: [{icon: "github", link: "https://github.com/echox-js/echox"}],
+    socialLinks: [{icon: "github", link: "https://github.com/blinkblinkhq/echox"}],
     footer: {
       message: "Released under the MIT License.",
       copyright: `Copyright © 2025-${new Date().getUTCFullYear()} Bairui SU`,
     },
-    logo: "/logo.svg",
+    logo: "/logo.png",
     search: {
       provider: "local",
     },
